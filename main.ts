9704655d6c36381bb57a4775f4ca3e581c2f0b9e
baptiste112 <77385444+baptiste112@users.noreply.github.com>
2021-01-13 11:11:54 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,11 @@
-input.onGesture(Gesture.TiltLeft, function () {
-    basic.showLeds(`
-        . # # # .
-        . # . # .
-        . # # . .
-        . # . # .
-        . # # # .
-        `)
+input.onButtonPressed(Button.A, function () {
+    stap += 1
 })
+input.onButtonPressed(Button.B, function () {
+    stap += -1
+})
+let stap = 0
+basic.showNumber(0)
 basic.forever(function () {
-	
+    basic.showNumber(stap)
 })
